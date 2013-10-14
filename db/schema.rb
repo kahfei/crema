f576188fa041f96bb43356f2f1cce3c8cceec916
kahfei <bak.kahfei@gmail.com>
2013-10-14 20:59:00 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919134633) do
+ActiveRecord::Schema.define(version: 20131002144611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20130919134633) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
