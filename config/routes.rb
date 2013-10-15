d39f0590cec944354ed8bd773d8b90300c5063e5
kahfei <bak.kahfei@gmail.com>
2013-10-15 11:20:01 +0800

    added session

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Crema::Application.routes.draw do
 
   resources :students
 
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
