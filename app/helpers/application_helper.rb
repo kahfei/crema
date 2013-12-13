7e9b14813c4d846211d91aec0eb2cc75a5fb69bf
kahfei <bak.kahfei@gmail.com>
2013-12-14 01:13:16 +0800

    payment due month in student view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	def ringgit(price)
+		number_to_currency(price, unit: "RM")
+	end
+
+	Student.class_eval do 
+		def due_month
+			if self.payments.empty?
+				Date.today
+			else
+				(self.payments.last.paid_for_month.to_i) + 1
+			end
+		end
+	end
 end
--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,10 @@
 module StudentsHelper
+
+	def due_month
+		if self.payments.empty?
+			Date.today
+		else
+			(self.payments.last.paid_for_month.to_i) + 1
+		end
+	end
 end
